3ccb77e3620630eba072d61d4629f2e84bbabede
Katarzyna <kkubikka@gmail.com>
2018-10-22 19:54:14 +0200

    adding dependencies

---
+++ src/main/java/twitter/Main.java
@@ -0,0 +1,16 @@
+package twitter;
+
+import org.apache.catalina.startup.Tomcat;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = create();
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    private static Tomcat create() throws Exception {
+        return EmbeddedTomcatFactory.create(8080);
+    }
+}
+
